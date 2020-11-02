d99fc8e15edf9a81d5412c06d0001dce18fcb818
hallef <hallef.vieira@hotmail.com>
2020-11-01 21:05:09 -0300

    Fundamentos de React Native

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { View, StatusBar } from 'react-native';
-import 'intl';
-import 'intl/locale-data/jsonp/pt-BR';
 
 import Routes from './routes';
+import AppContainer from './hooks';
 
-const App: React.FC = () => {
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+const App: React.FC = () => (
+  <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+    <AppContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
       <Routes />
-    </View>
-  );
-};
+    </AppContainer>
+  </View>
+);
 
 export default App;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,132 +1,65 @@
-import styled, { css } from 'styled-components/native';
+import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-interface CategoryItemProps {
-  isSelected?: boolean;
+interface Product {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
 }
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
   flex: 1;
+  align-items: center;
+`;
+
+export const ProductContainer = styled.View`
+  border-radius: 5px;
+  margin-top: 60px;
+  flex: 1;
+  flex-direction: row;
+`;
+
+export const ProductList = styled(
+  FlatList as new () => FlatList<Product>,
+).attrs({
+  numColumns: 2,
+})`
+  flex: 1;
+  padding: 0 10px;
+`;
+
+export const Product = styled.View`
   background: #fff;
-`;
-
-export const Header = styled.View`
-  padding: 60px 24px 60px;
-  background: #c72828;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-export const FilterContainer = styled.View`
-  padding: 0 24px;
-  margin-top: -28px;
-`;
-
-export const Title = styled.Text`
-  font-family: 'Poppins-Regular';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 30px;
-  color: #3d3d4d;
-  padding: 0 20px;
-`;
-
-export const CategoryContainer = styled.View`
-  margin-top: 40px;
-`;
-
-export const CategorySlider = styled.ScrollView`
-  margin-top: 16px;
-`;
-
-export const CategoryItem = styled.TouchableOpacity<CategoryItemProps>`
-  background-color: #f0f0f5;
-  border: 2px;
-  border-color: #f0f0f5;
-  height: 120px;
-  width: 120px;
-  border-radius: 8px;
-  padding-top: 20px;
-  padding-bottom: 16px;
-  margin-right: 8px;
-  align-items: center;
-  justify-content: space-between;
-  text-align: center;
-  ${props =>
-    props.isSelected &&
-    css`
-      border-color: #c72828;
-      background-color: #ffebeb;
-    `}
-`;
-
-export const CategoryItemTitle = styled.Text`
-  font-style: normal;
-  font-weight: bold;
-  font-size: 15px;
-  line-height: 15px;
-  text-align: center;
-  color: #6c6c80;
-`;
-
-export const FoodsContainer = styled.View`
-  margin-top: 40px;
-`;
-
-export const FoodList = styled.View`
-  padding: 0 20px;
-  margin-top: 16px;
-`;
-
-export const Food = styled.TouchableOpacity`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background: #f0f0f5;
-  border-radius: 8px;
-  margin-bottom: 16px;
-`;
-
-export const FoodImageContainer = styled.View`
-  background: #ffb84d;
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-  padding: 16px;
-  height: 100%;
-`;
-
-export const FoodContent = styled.View`
+  padding: 16px 16px;
+  border-radius: 5px;
+  margin: 8px;
   flex: 1;
-  padding: 16px;
 `;
 
-export const FoodTitle = styled.Text`
-  font-family: 'Poppins-Regular';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 15px;
-  line-height: 22px;
-  color: #3d3d4d;
-`;
-export const FoodDescription = styled.Text`
-  font-family: 'Poppins-Regular';
-  font-style: normal;
-  font-weight: normal;
-  font-size: 10px;
-  line-height: 16px;
-  margin-top: 6px;
-  color: #3d3d4d;
+export const ProductImage = styled.Image`
+  height: 122px;
+  width: 122px;
+  align-self: center;
 `;
 
-export const FoodPricing = styled.Text`
-  font-family: 'Poppins-Regular';
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 21px;
-  margin-top: 8px;
-  font-weight: 600;
-  color: #39b100;
+export const ProductTitle = styled.Text`
+  font-size: 14px;
+  margin-top: 10px;
 `;
+
+export const PriceContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 10px;
+  margin-top: auto;
+`;
+
+export const ProductPrice = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+  color: #e83f5b;
+`;
+
+export const ProductButton = styled.TouchableOpacity``;
